9b64f4e3d7ee15d3d83233d9812d615ee4295d60
netanlb <netanlb@gmail.com>
2022-12-25 19:55:35 +0200

    started working on expense list

--- src/pages/expanses/components/expenses-list/expenses-list.jsx
+++ src/pages/expanses/components/expenses-list/expenses-list.jsx
@@ -1,5 +1,154 @@
-import { Container } from "@mui/material";
+import { Container, Box, Typography } from "@mui/material";
+import { ExpenseItem } from "./expenses-list.styled";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+
+const expenseItems = [
+  {
+    name: "Rent",
+    amount: 1000,
+    date: "2021-09-01",
+    category: "Home",
+    description: "Rent for the month of September",
+  },
+  {
+    name: "Groceries",
+    amount: 100,
+    date: "2021-09-01",
+    category: "Food",
+    description: "Groceries for the month of September",
+  },
+  {
+    name: "Car Insurance",
+    amount: 100,
+    date: "2021-09-01",
+    category: "Insurance",
+    description: "Car Insurance for the month of September",
+  },
+  {
+    name: "Car Insurance",
+    amount: 100,
+    date: "2021-09-01",
+    category: "Insurance",
+    description: "Car Insurance for the month of September",
+  },
+  {
+    name: "Food for Cat",
+    amount: 40,
+    date: "2021-09-01",
+    category: "Pet",
+    description: "Food for Cat for the month of September",
+  },
+  {
+    name: "Food for Dog",
+    amount: 40,
+    date: "2021-09-01",
+    category: "Pet",
+    description: "Food for Dog for the month of September",
+  },
+  {
+    name: "Car Insurance",
+    amount: 100,
+    date: "2021-09-01",
+    category: "Insurance",
+    description: "Car Insurance for the month of September",
+  },
+  {
+    name: "Car Insurance",
+    amount: 100,
+    date: "2021-09-01",
+    category: "Insurance",
+    description: "Car Insurance for the month of September",
+  },
+  {
+    name: "Food for Cat",
+    amount: 40,
+    date: "2021-09-01",
+    category: "Pet",
+    description: "Food for Cat for the month of September",
+  },
+  {
+    name: "Food for Dog",
+    amount: 40,
+    date: "2021-09-01",
+    category: "Pet",
+    description: "Food for Dog for the month of September",
+  },
+  {
+    name: "Car Insurance",
+    amount: 100,
+    date: "2021-09-01",
+    category: "Insurance",
+    description: "Car Insurance for the month of September",
+  },
+  {
+    name: "Food for Cat",
+    amount: 40,
+    date: "2021-09-01",
+    category: "Pet",
+    description: "Food for Cat for the month of September",
+  },
+  {
+    name: "Food for Dog",
+    amount: 40,
+    date: "2021-09-01",
+    category: "Pet",
+    description: "Food for Dog for the month of September",
+  },
+  {
+    name: "Food for Dog",
+    amount: 40,
+    date: "2021-09-01",
+    category: "Pet",
+    description: "Food for Dog for the month of September",
+  },
+  {
+    name: "Car Insurance",
+    amount: 100,
+    date: "2021-09-01",
+    category: "Insurance",
+    description: "Car Insurance for the month of September",
+  },
+  {
+    name: "Food for Cat",
+    amount: 40,
+    date: "2021-09-01",
+    category: "Pet",
+    description: "Food for Cat for the month of September",
+  },
+  {
+    name: "Food for Dog",
+    amount: 40,
+    date: "2021-09-01",
+    category: "Pet",
+    description: "Food for Dog for the month of September",
+  },
+];
 
 export default function ExpenseListComponent() {
-  return <Container>Expense List</Container>;
+  return (
+    <Container>
+      {expenseItems.map((item) => (
+        <ExpenseItem>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+            }}
+          >
+            <ShoppingCartIcon
+              sx={{ marginRight: ".2em", fontSize: "2.7em" }}
+            ></ShoppingCartIcon>
+            <div>
+              <div style={{ fontSize: "1.2em", color: "var(--purple-light)" }}>
+                {item.name}
+              </div>
+              <div style={{ fontSize: ".8em", color: "gray" }}>{item.date}</div>
+            </div>
+          </Box>
+          <div style={{ fontSize: "1.6em" }}>{item.amount}$</div>
+        </ExpenseItem>
+      ))}
+    </Container>
+  );
 }
---
+++ src/pages/expanses/components/expenses-list/expenses-list.styled.jsx
@@ -0,0 +1,8 @@
+import { styled, Box } from "@mui/material";
+
+export const ExpenseItem = styled(Box)(() => ({
+  marginTop: ".5em",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "space-between",
+}));
