e18c0e8b64aeb21e45c01fc6042c1ef0818fea1c
netanlb <netanlb@gmail.com>
2022-12-25 20:35:58 +0200

    added icons

--- src/pages/expanses/components/expenses-list/expenses-list.jsx
+++ src/pages/expanses/components/expenses-list/expenses-list.jsx
@@ -1,132 +1,92 @@
 import { Container, Box, Typography } from "@mui/material";
 import { ExpenseItem } from "./expenses-list.styled";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import FastfoodRoundedIcon from "@mui/icons-material/FastfoodRounded";
+import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
+import ChairRoundedIcon from "@mui/icons-material/ChairRounded";
+import RestaurantRoundedIcon from "@mui/icons-material/RestaurantRounded";
+import TheaterComedyIcon from "@mui/icons-material/TheaterComedy";
+
+const icons = {
+  groceries: (
+    <ShoppingCartIcon
+      sx={{ color: "green", marginRight: ".5em", fontSize: "2.3em" }}
+    ></ShoppingCartIcon>
+  ),
+  home: (
+    <HomeRoundedIcon
+      sx={{ color: "purple", marginRight: ".5em", fontSize: "2.3em" }}
+    ></HomeRoundedIcon>
+  ),
+
+  furniture: (
+    <ChairRoundedIcon
+      sx={{ color: "brown", marginRight: ".5em", fontSize: "2.3em" }}
+    ></ChairRoundedIcon>
+  ),
+  food: (
+    <FastfoodRoundedIcon
+      sx={{ color: "orange", marginRight: ".5em", fontSize: "2.3em" }}
+    ></FastfoodRoundedIcon>
+  ),
+};
 
 const expenseItems = [
   {
     name: "Rent",
     amount: 1000,
     date: "2021-09-01",
-    category: "Home",
+    category: "home",
     description: "Rent for the month of September",
   },
   {
-    name: "Groceries",
+    name: "groceries",
     amount: 100,
     date: "2021-09-01",
-    category: "Food",
+    category: "food",
     description: "Groceries for the month of September",
   },
+
   {
-    name: "Car Insurance",
-    amount: 100,
-    date: "2021-09-01",
-    category: "Insurance",
-    description: "Car Insurance for the month of September",
-  },
-  {
-    name: "Car Insurance",
-    amount: 100,
-    date: "2021-09-01",
-    category: "Insurance",
-    description: "Car Insurance for the month of September",
-  },
-  {
-    name: "Food for Cat",
+    name: "Bed",
     amount: 40,
     date: "2021-09-01",
-    category: "Pet",
-    description: "Food for Cat for the month of September",
+    category: "furniture",
+    description: "Bed for new apartment",
   },
   {
-    name: "Food for Dog",
+    name: "Hamurger",
     amount: 40,
     date: "2021-09-01",
-    category: "Pet",
-    description: "Food for Dog for the month of September",
+    category: "food",
+    description: "Hamburger at GDB in Tel Aviv",
   },
   {
     name: "Car Insurance",
     amount: 100,
     date: "2021-09-01",
-    category: "Insurance",
-    description: "Car Insurance for the month of September",
-  },
-  {
-    name: "Car Insurance",
-    amount: 100,
-    date: "2021-09-01",
-    category: "Insurance",
+    category: "home",
     description: "Car Insurance for the month of September",
   },
   {
     name: "Food for Cat",
     amount: 40,
     date: "2021-09-01",
-    category: "Pet",
+    category: "home",
     description: "Food for Cat for the month of September",
   },
   {
     name: "Food for Dog",
     amount: 40,
     date: "2021-09-01",
-    category: "Pet",
-    description: "Food for Dog for the month of September",
-  },
-  {
-    name: "Car Insurance",
-    amount: 100,
-    date: "2021-09-01",
-    category: "Insurance",
-    description: "Car Insurance for the month of September",
-  },
-  {
-    name: "Food for Cat",
-    amount: 40,
-    date: "2021-09-01",
-    category: "Pet",
-    description: "Food for Cat for the month of September",
-  },
-  {
-    name: "Food for Dog",
-    amount: 40,
-    date: "2021-09-01",
-    category: "Pet",
-    description: "Food for Dog for the month of September",
-  },
-  {
-    name: "Food for Dog",
-    amount: 40,
-    date: "2021-09-01",
-    category: "Pet",
-    description: "Food for Dog for the month of September",
-  },
-  {
-    name: "Car Insurance",
-    amount: 100,
-    date: "2021-09-01",
-    category: "Insurance",
-    description: "Car Insurance for the month of September",
-  },
-  {
-    name: "Food for Cat",
-    amount: 40,
-    date: "2021-09-01",
-    category: "Pet",
-    description: "Food for Cat for the month of September",
-  },
-  {
-    name: "Food for Dog",
-    amount: 40,
-    date: "2021-09-01",
-    category: "Pet",
+    category: "home",
     description: "Food for Dog for the month of September",
   },
 ];
 
 export default function ExpenseListComponent() {
   return (
-    <Container>
+    <Box>
       {expenseItems.map((item) => (
         <ExpenseItem>
           <Box
@@ -134,21 +94,26 @@ export default function ExpenseListComponent() {
               display: "flex",
               flexDirection: "row",
               alignItems: "center",
+              justifyContent: "center",
             }}
           >
-            <ShoppingCartIcon
-              sx={{ marginRight: ".2em", fontSize: "2.7em" }}
-            ></ShoppingCartIcon>
+            {icons[item.category]}
             <div>
-              <div style={{ fontSize: "1.2em", color: "var(--purple-light)" }}>
+              <div
+                style={{
+                  fontSize: "1.1em",
+                  fontWeight: 600,
+                  color: "var(--purple-light)",
+                }}
+              >
                 {item.name}
               </div>
               <div style={{ fontSize: ".8em", color: "gray" }}>{item.date}</div>
             </div>
           </Box>
-          <div style={{ fontSize: "1.6em" }}>{item.amount}$</div>
+          <div style={{ fontSize: "1.5em" }}>{item.amount}$</div>
         </ExpenseItem>
       ))}
-    </Container>
+    </Box>
   );
 }
--- src/pages/expanses/components/expenses-list/expenses-list.styled.jsx
+++ src/pages/expanses/components/expenses-list/expenses-list.styled.jsx
@@ -1,8 +1,13 @@
-import { styled, Box } from "@mui/material";
+import { styled, Container } from "@mui/material";
 
-export const ExpenseItem = styled(Box)(() => ({
-  marginTop: ".5em",
+export const ExpenseItem = styled(Container)(() => ({
+  paddingTop: ".5em",
+  paddingBottom: ".5em",
   display: "flex",
   alignItems: "center",
   justifyContent: "space-between",
+  "&:hover": {
+    background: "var(--off-white-darker)",
+    cursor: "pointer",
+  },
 }));
