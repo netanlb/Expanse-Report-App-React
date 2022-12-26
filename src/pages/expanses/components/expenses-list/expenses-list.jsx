4c50a4867f4526294aca27d86fd80bb9ed747c0f
Haim Atiya <haim.attiya@gmail.com>
2022-12-26 19:41:15 +0200

    Data filter

--- src/pages/expanses/components/expenses-list/expenses-list.jsx
+++ src/pages/expanses/components/expenses-list/expenses-list.jsx
@@ -6,88 +6,38 @@ import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
 import ChairRoundedIcon from "@mui/icons-material/ChairRounded";
 import RestaurantRoundedIcon from "@mui/icons-material/RestaurantRounded";
 import TheaterComedyIcon from "@mui/icons-material/TheaterComedy";
+import {getExpanses, addExpense, deleteExpense, getFilterOptions} from "../../../../backendService/backend";
+
 
 const icons = {
-  groceries: (
+  Energy: (
     <ShoppingCartIcon
       sx={{ color: "green", marginRight: ".5em", fontSize: "2.3em" }}
     ></ShoppingCartIcon>
   ),
-  home: (
+  Home: (
     <HomeRoundedIcon
       sx={{ color: "purple", marginRight: ".5em", fontSize: "2.3em" }}
     ></HomeRoundedIcon>
   ),
 
-  furniture: (
+  Entertainment: (
     <ChairRoundedIcon
       sx={{ color: "brown", marginRight: ".5em", fontSize: "2.3em" }}
     ></ChairRoundedIcon>
   ),
-  food: (
+  Food: (
     <FastfoodRoundedIcon
       sx={{ color: "orange", marginRight: ".5em", fontSize: "2.3em" }}
     ></FastfoodRoundedIcon>
   ),
 };
 
-const expenseItems = [
-  {
-    name: "Rent",
-    amount: 1000,
-    date: "2021-09-01",
-    category: "home",
-    description: "Rent for the month of September",
-  },
-  {
-    name: "groceries",
-    amount: 100,
-    date: "2021-09-01",
-    category: "food",
-    description: "Groceries for the month of September",
-  },
 
-  {
-    name: "Bed",
-    amount: 40,
-    date: "2021-09-01",
-    category: "furniture",
-    description: "Bed for new apartment",
-  },
-  {
-    name: "Burger",
-    amount: 40,
-    date: "2021-09-01",
-    category: "food",
-    description: "Burger at GDB in Tel Aviv",
-  },
-  {
-    name: "Car Insurance",
-    amount: 100,
-    date: "2021-09-01",
-    category: "home",
-    description: "Car Insurance for the month of September",
-  },
-  {
-    name: "Rami Levi",
-    amount: 40,
-    date: "2021-09-01",
-    category: "groceries",
-    description: "Food for the week",
-  },
-  {
-    name: "Food for Dog",
-    amount: 40,
-    date: "2021-09-01",
-    category: "home",
-    description: "Food for Dog for the month of September",
-  },
-];
-
-export default function ExpenseListComponent() {
+export default function ExpenseListComponent({currentFilters}) {
   return (
     <Box>
-      {expenseItems.map((item) => (
+      {getExpanses(currentFilters).map((item) => (
         <ExpenseItem>
           <Box
             sx={{
@@ -111,7 +61,7 @@ export default function ExpenseListComponent() {
               <div style={{ fontSize: ".8em", color: "gray" }}>{item.date}</div>
             </div>
           </Box>
-          <div style={{ fontSize: "1.5em" }}>{item.amount}$</div>
+          <div style={{ fontSize: "1.5em" }}>{item.sum}$</div>
         </ExpenseItem>
       ))}
     </Box>
