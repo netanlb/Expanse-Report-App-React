a25e4f3f840b4eb2011940163e2f22d3541b2414
Haim Atiya <haim.attiya@gmail.com>
2022-12-26 12:49:53 +0200

    filterFunction

--- src/pages/expanses/components/expenses-toolbar/expenses-toolbar.jsx
+++ src/pages/expanses/components/expenses-toolbar/expenses-toolbar.jsx
@@ -1,13 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import {
   ToolbarContainer,
   FilterBox,
   AddExpenseButton,
+  RangeContainer,
+  RangeTooltip,
+  RangeInput,
 } from "./expenses-toolbar.styled";
 import FilterListIcon from "@mui/icons-material/FilterList";
 import AddIcon from "@mui/icons-material/Add";
 import { Typography } from "@mui/material";
-
 export default function ExpenseToolbarComponent() {
   const years = [2022, 2021, 2020, 2019, 2018, 2017, 2016];
   const months = [
@@ -35,7 +37,9 @@ export default function ExpenseToolbarComponent() {
   const [year, setYear] = useState(0);
   const [month, setMonth] = useState(0);
   const [category, setCategory] = useState(0);
-
+  const [startSum, setStartSum] = useState(0);
+  const [endSum, setEndSum] = useState(10000);
+  const [rangeTooltip, setRangeTooltip] = useState(false);
   const yearChange = (event) => {
     setYear(event.target.value);
   };
@@ -45,13 +49,44 @@ export default function ExpenseToolbarComponent() {
   const categoryChange = (event) => {
     setCategory(event.target.value);
   };
+  const startSumChange = (event) => {
+    setStartSum(event.target.value);
+  };
+  const endSumChange = (event) => {
+    setEndSum(event.target.value);
+  };
   const clearValues = () => {
     // 👇️ clear input value
-    setYear('0');
-    setMonth('0');
-    setCategory('0');
+    setYear("0");
+    setMonth("0");
+    setCategory("0");
+    setStartSum(0);
+    setEndSum(10000);
   };
-
+  const rangeChange = (event) => {
+    setRangeTooltip(!rangeTooltip);
+  };
+  const RangeTooltipRef = useRef(null);
+  const CloseTooltip = (e) => {
+    if (
+      RangeTooltipRef.current &&
+      rangeTooltip &&
+      !RangeTooltipRef.current.contains(e.target)
+    ) {
+      setRangeTooltip(false);
+    }
+  };
+  const applyFilter = () => {
+    let filterObject = {
+      year: year,
+      month: month,
+      category: category,
+      startSum: startSum,
+      endSum: endSum,
+    };
+    console.log(filterObject);
+  };
+  document.addEventListener("mousedown", CloseTooltip);
   return (
     <ToolbarContainer>
       <FilterBox>
@@ -86,8 +121,31 @@ export default function ExpenseToolbarComponent() {
             </option>
           ))}
         </select>
+        <RangeContainer disableGutters>
+          <button ref={RangeTooltipRef} onClick={rangeChange}>
+            Range
+          </button>
+          <RangeTooltip
+            ref={RangeTooltipRef}
+            display={rangeTooltip ? "flex" : "none"}
+          >
+            <RangeInput
+              type="number"
+              onChange={startSumChange}
+              value={startSum}
+              placeholder="Start Sum"
+            />
+            <RangeInput
+              type="number"
+              onChange={endSumChange}
+              value={endSum}
+              placeholder="End Sum"
+            />
+          </RangeTooltip>
+        </RangeContainer>
+
         <button onClick={clearValues}>Clear</button>
-        <button>Apply</button>
+        <button onClick={applyFilter}>Apply</button>
       </FilterBox>
       <AddExpenseButton variant="contained">
         <AddIcon></AddIcon>
@@ -100,19 +158,3 @@ export default function ExpenseToolbarComponent() {
     </ToolbarContainer>
   );
 }
-
-/*
-        <select>
-          <option disabled key="months">Months</option>
-          {months.map((month) => (
-            <option value={month} key={month}>{month}</option>
-          ))}
-        </select>
-        <select>
-          <option disabled key="categories">Categories</option>
-          {categories.map((category) => (
-            <option value={category} key={category}>{category}</option>
-          ))}
-        </select>
-
-*/
--- src/pages/expanses/components/expenses-toolbar/expenses-toolbar.styled.jsx
+++ src/pages/expanses/components/expenses-toolbar/expenses-toolbar.styled.jsx
@@ -26,3 +26,23 @@ export const AddExpenseButton = styled(Button)(() => ({
     background: "var(--purple-hover)",
   },
 }));
+
+export const RangeContainer = styled(Container)(() => ({
+  position: "relative",
+}));
+
+export const RangeTooltip = styled(Box)(() => ({
+  position: "absolute",
+  top: "25px",
+  width:"260px",
+  height: "50px",
+  backgroundColor: "white",
+  border: "1px solid black",
+  borderRadius: "10px",
+}));
+
+export const RangeInput = styled("input")(() => ({
+  width:"100px",
+  height: "20px",
+  margin: "10px",
+}));
