aeea562e8aa0105b75af7b27c1001317130d8d95
Haim Atiya <haim.attiya@gmail.com>
2023-01-01 21:43:35 +0200

    Add style to details component

--- src/pages/expanses/components/expense-details/specific-details/specific-details.jsx
+++ src/pages/expanses/components/expense-details/specific-details/specific-details.jsx
@@ -1,5 +1,21 @@
 import { Container } from "@mui/material";
+import {
+  ExpenseHeader,
+  backIcon,
+  sumText,
+  CatDateWrapper,
+  MapWrapper,
+  mapStyle,
+  BottomButtons,
+  Description
+} from "./specific-details.styled";
 import { customScroll } from "../../../expanses.styled";
+import { icons } from "../../expenses-list/expenses-list";
+import { Button } from "@mui/material";
+import IconButton from "@mui/material/IconButton";
+import CancelIcon from "@mui/icons-material/Cancel";
+import EventIcon from "@mui/icons-material/Event";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 export default function SpecificExpenseDetails({
   chosenExpense,
@@ -15,28 +31,72 @@ export default function SpecificExpenseDetails({
   }
 
   return (
-    <Container sx={{ background: "var(--off-white-darker)", ...customScroll }}>
-      <button
-        onClick={() => {
-          setChosenExpense();
+    <div style={{overflow:"hidden"}}>
+      <ExpenseHeader>
+        <iconButton
+          style={backIcon}
+          onClick={() => {
+            setChosenExpense();
+          }}
+        >
+          <CancelIcon />
+        </iconButton>
+        <span>Expense details</span>
+      </ExpenseHeader>
+      <Container
+        sx={{
+          background: "var(--off-white-darker)",
+          textAlign: "center",
+          ...customScroll,
+          height: "100%",
         }}
       >
-        Back
-      </button>
-      <h1>Expense Details</h1>
-      <h1>{chosenExpense.name}</h1>
-      <h2>{chosenExpense.category}</h2>
-      <h3>{chosenExpense.description}</h3>
-      <h4>{chosenExpense.sum}₪</h4>
-      <h5>{formatDate(chosenExpense.date)}</h5>
-      <img src="./map.jpg" alt="map" />
-      <button
-        onClick={() => {
-          deleteExpense(chosenExpense.id);
-        }}
-      >
-        Delete
-      </button>
-    </Container>
+        <h1>{chosenExpense.name}</h1>
+        <h2 style={sumText}>{chosenExpense.sum}₪</h2>
+        <CatDateWrapper>
+          <div>
+            {icons[chosenExpense.category]}
+            {chosenExpense.category}
+          </div>
+
+          <div>
+            <EventIcon
+              sx={{
+                color: "var(--purple-light)",
+                fontSize: "2.3em",
+                marginRight: ".5em",
+              }}
+            />
+            {formatDate(chosenExpense.date)}
+          </div>
+        </CatDateWrapper>
+        {(chosenExpense.description)  &&
+        <Description>{chosenExpense.description}</Description>
+      }
+        
+
+        
+        <MapWrapper>
+          <img src="./map.jpg" alt="map" style={mapStyle} />
+        </MapWrapper>
+        <BottomButtons>
+          <Button
+            variant="contained"
+            color="error"
+            startIcon={<DeleteIcon />}
+            sx={{ marginTop: "20px" }}
+            onClick={() => {
+              deleteExpense(chosenExpense.id);
+            }}
+          >
+            Delete Expense
+          </Button>
+        </BottomButtons>
+      </Container>
+    </div>
   );
 }
+
+/*
+      
+*/
---
+++ src/pages/expanses/components/expense-details/specific-details/specific-details.styled.jsx
@@ -0,0 +1,80 @@
+import { IconButton, styled } from "@mui/material";
+import { Container } from "@mui/system";
+import { Button } from "@mui/material";
+
+export const backIcon = {
+  float: "left",
+}
+
+export const ExpenseHeader = styled(Container)(() => ({
+    width: "100%",
+    height: "40px",
+    display: "flex",
+    alignItems: "center",
+    backgroundColor: "var(--purple-dark)",
+    opacity: 0.5,
+    margin: 0,
+    textAlign: "center",
+    span:{
+      margin: "0 auto",
+      color: "white",
+      fontFamily: '"Roboto","Helvetica","Arial",sans-serif',
+      fontWeight: 400,
+      fontSize: "1em",
+      lineHight: 1.5,
+    },
+    "IconButton": {
+      color: "white",
+    }
+}));
+
+export const sumText = { 
+  fontSize: "50px",
+  fontWeight: 500,
+  color: "var(--purple-dark)",
+  padding: "0px",
+  margin: "0px"
+}
+
+export const CatDateWrapper = styled(Container)(() => ({
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+  width: "100%",
+  padding: "0px",
+  margin: "30px 0 20px 0",
+  div: {
+    fontSize: "14px",
+    fontWeight: 0,
+    width:"30%",
+    display: "flex",
+  alignItems: "center", 
+  backgroundColor: "var(--purple-light-opacity-20)",
+  padding: "10px",
+  borderRadius: "10px",
+  color: "black",
+  }
+}));
+
+export const MapWrapper = styled(Container)(() => ({
+  width: "100%",
+}));
+export const mapStyle = {
+  width: "300px",
+  height: "300px",
+  borderRadius: "10px",
+  boxShadow: "0px 0px 50px 0px var(--purple-light-opacity-20)",
+}
+
+export const BottomButtons = styled(Container)(() => ({
+  width: "100%",
+}));
+
+export const Description = styled(Container)(() => ({
+  width: "90%",
+  display:"block",
+  backgroundColor: "var(--purple-light-opacity-20)",
+  padding: "10px",
+  borderRadius: "10px",
+  marginBottom: "20px",
+}));
